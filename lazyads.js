9dfa9a8218f4b781292c3883f7a0381faa927e3d
mrvoxie <63644656+mrvoxie@users.noreply.github.com>
2020-04-20 23:11:34 +0700

    Update lazyads.js

--- lazyads.js
+++ lazyads.js
@@ -1,1 +1,10 @@
-var lazyadsense = false; window.addEventListener("scroll", function(){ if ((document.documentElement.scrollTop != 0 && lazyadsense === false) || (document.body.scrollTop != 0 && lazyadsense === false)) { (function() { var ad = document.createElement('script'); ad.type = 'text/javascript'; ad.async = true; ad.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'; var sc = document.getElementsByTagName('script')[0]; sc.parentNode.insertBefore(ad, sc); })(); document.body.className += " sticky"; lazyadsense = true;}}, true);
+var lazyadsense = false; 
+window.addEventListener("scroll", function(){ 
+  if ((document.documentElement.scrollTop != 0 && lazyadsense === false) || (document.body.scrollTop != 0 && lazyadsense === false)) { 
+    (function() { var ad = document.createElement('script'); ad.type = 'text/javascript'; ad.async = true; ad.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'; var sc = document.getElementsByTagName('script')[0]; sc.parentNode.insertBefore(ad, sc); })(); 
+    document.body.className += " sticky"; lazyadsense = true;}}, true);
+
+     (adsbygoogle = window.adsbygoogle || []).push({
+          google_ad_client: "ca-pub-4901031377837473",
+          enable_page_level_ads: true
+     });
